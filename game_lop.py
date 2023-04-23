4e8ef73b02a6de1b873d28202eaff16cfd9e4121
W. K. Stump <wkschessman@gmail.com>
2023-04-22 20:02:13 -0400

    Changing csv file

--- game_lop.py
+++ game_lop.py
@@ -2,11 +2,11 @@ import csv
 from ortools.linear_solver import pywraplp
 
 # opening the CSV file
-with open('Game Test.csv', mode='r')as file:
+with open('BGG Top 100.csv', mode='r')as file:
 
     # reading the CSV file
     csvFile = csv.reader(file)
 
     # displaying the contents of the CSV file
     for lines in csvFile:
-        print(lines)
+        print(lines[0])
